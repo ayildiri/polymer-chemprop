48fb19e410dd855e0349744d2a00bb14dd2f9033
ayildiri <95155708+ayildiri@users.noreply.github.com>
2025-03-22 22:09:41 -0700

    Update ssl_pretrain_V2.py

--- chemprop/train/ssl_pretrain_V2.py
+++ chemprop/train/ssl_pretrain_V2.py
@@ -306,6 +306,7 @@ train_args.phase_features_path = None
 train_args.atom_descriptors = None
 train_args.atom_descriptors_path = None
 train_args.bond_descriptors = None
+train_args.bond_features_path = None
 train_args.max_data_size = None
 train_args.cache_cutoff = 1e7
 train_args.no_cache = True
@@ -314,6 +315,7 @@ train_args.target_columns = []
 train_args.number_of_molecules = 1
 train_args.mol_cache_path = None
 train_args.skip_invalid_smiles = True
+
 
 train_data = get_data(path=data_path, args=train_args, skip_none_targets=True)
 # Filter train and val sets within train_data based on our indices
